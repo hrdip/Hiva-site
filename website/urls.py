7ce224346f15a520ac67180bb968fbf6723c3ba6
hrdip <hrdip.2018@gmail.com>
2023-10-24 16:26:18 +0800

    added html template
    +added html template
    +STATIC_ROOT
    +MEDIA_ROOT
    +linked html to static file with jinja
    +dynamic url

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view),
-    path('about', about_view),
-    path('contact', contact_view),
+    path('', index_view, name='index'),
+    path('about', about_view, name='about'),
+    path('contact', contact_view, name='contact'),
 ]
