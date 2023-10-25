3ed4a398dbef5e567b8089cecc86eb0366b37e62
hrdip <hrdip.2018@gmail.com>
2023-10-26 01:10:12 +0800

    customizing admin panel
    customizing admin panel with models
    + meta class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'created_date', 'status', 'published_date')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+admin.site.register(Post,PostAdmin) 
--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,11 @@ class Post(models.Model):
     # category
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
-    published = models.DateTimeField(null=True)
+    published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('-created_date',)
+    def __str__(self):
+        return "{}-{}".format(self.title, self.id)
