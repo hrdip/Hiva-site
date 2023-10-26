f315320e7d52f9dc9476a6ed488d8df1752dab30
hrdip <hrdip.2018@gmail.com>
2023-10-26 19:00:30 +0800

    connection blog and single_blog
    dynamic post in blog and single_blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
 
 def blog_view (request):
-    return render(request,'blog/blog_home.html')
+    posts = Post.objects.filter(status=1)
+    context={'posts': posts}
+    return render(request,'blog/blog_home.html', context)
 
-def blog_single (request):
-    return render(request,'blog/blog_single.html')
- 
+def blog_single (request,pid):
+    posts = get_object_or_404(Post,pk=pid)
+    context={'post': posts}
+    return render(request,'blog/blog_single.html', context)
+
