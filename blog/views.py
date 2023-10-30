17b92358237e0022157a76c47880d125c958e4a7
hrdip <hrdip.2018@gmail.com>
2023-10-30 15:13:24 +0800

    submitted sum filter
    + filtering by categories
    + filtering by authors
    + filtering by searching

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 
-def blog_view (request):
+def blog_view (request,**kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username']) 
     context={'posts': posts}
     return render(request,'blog/blog_home.html', context)
 
@@ -12,3 +16,17 @@ def blog_single (request,pid):
     context={'post': post}
     return render(request,'blog/blog_single.html', context)
 
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name) 
+    context={'posts': posts}
+    return render(request,'blog/blog_home.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts =posts.filter(content__contains=s)
+            
+    context={'posts': posts}
+    return render(request,'blog/blog_home.html', context)
