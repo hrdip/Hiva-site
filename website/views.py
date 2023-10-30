31a726ba6af0e6a33afc611c862a2f1ae51b8e45
hrdip <hrdip.2018@gmail.com>
2023-10-31 01:06:13 +0800

    POST request
    submitted:
    contact forms
    newsletter forms

--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from website.models import Contact, Newsletter
+from website.forms import ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view (request):
     return render(request,'website/index.html')
@@ -6,5 +10,25 @@ def index_view (request):
 def about_view (request):
     return render(request,'website/about.html')
 
+
 def contact_view (request):
-    return render(request,'website/contact.html') 
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submitted successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket didnt submitted')
+    form = ContactForm()
+    context = {'form':form}
+    return render(request,'website/contact.html',context)
+
+def newsletter_view (request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+    
