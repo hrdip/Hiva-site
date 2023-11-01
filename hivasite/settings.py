0d621e0b961947e9cc5bd48b4ca9a0bc9fedfcec
hrdip <hrdip.2018@gmail.com>
2023-11-02 01:21:04 +0800

    robots

--- hivasite/settings.py
+++ hivasite/settings.py
@@ -40,12 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'robots',
+    'debug_toolbar',
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blog'
 ]
 
+# sites framework
 SITE_ID = 1
+
+# robots
+ROBOTS_USE_HOST = False
+ROBOTS_USE_SITEMAP = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'hivasite.urls'
@@ -141,3 +149,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
